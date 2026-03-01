17941a5c86908ecb86c4e266fc8eb816a5a5783e
Julián MR <julian.marin42@eia.edu.co>
2026-03-01 17:08:10 -0500

    Clase Avion lista + inicio Aerolinea (atributos y constructor)

--- src/principal/Aerolinea.java
+++ src/principal/Aerolinea.java
@@ -1,3 +1,26 @@
 package principal;
 
-public class Aerolinea {}
+import java.util.Arrays;
+
+public class Aerolinea {
+	private String nombre;
+	private Aeropuerto[] aeropuertos;
+	private Avion[] aviones;
+	private Cliente[] clientes;
+	private Empleado[] empleados;
+	private Vuelo[] vuelos;
+	
+	public Aerolinea(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	//falta: add, list, delete para los Arrays
+}
--- src/principal/Avion.java
+++ src/principal/Avion.java
@@ -1,3 +1,49 @@
 package principal;
 
-public class Avion {}
+public class Avion {
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private int capacidad;//cantidad de pasajeros que puede llevar
+	private boolean disponible;
+	
+	public Avion(String matricula, String marca, String modelo, int capacidad, boolean disponible) {
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.capacidad = capacidad;
+		this.disponible = disponible;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+}
