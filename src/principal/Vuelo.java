9882ac6cc3cc3c251ad11299d1e8f8cc7e0d3bf1
Daviiiid <david.giraldo52@eia.edu.co>
2026-03-01 16:14:31 -0500

    feat: Proyecto en Maven e Inicio de Vuelo

--- src/principal/Vuelo.java
+++ src/principal/Vuelo.java
@@ -1,5 +1,58 @@
+// Codigo sin excepciones
+
 package principal;
 
-public class Vuelo {
+import java.time.LocalDateTime;
 
+public class Vuelo {
+    private final String id;
+    private final String numVuelo;
+    private final Aeropuerto origen, destino;
+    private LocalDateTime fechaHoraSalida;
+    private LocalDateTime fechaHoraLlegada;
+    private Avion avion;
+    private String estadoVuelo; //enum?
+    private String puertaEmbarque;
+    private TripulanteCabina[] tripulacion;
+    private Piloto[] pilotos;
+    private Reserva[] reservas;
+
+    // Constructor
+    public Vuelo(Aeropuerto origen, Aeropuerto destino, LocalDateTime fechaHoraSalida, Avion avion,TripulanteCabina[] tripulacion, Piloto[] pilotos){
+        this.id = null; //TODO
+        this.numVuelo = null; //TODO
+        this.origen = origen;
+        this.destino = destino;
+        this.fechaHoraSalida = fechaHoraSalida;
+        this.fechaHoraLlegada = null; //TODO
+        this.avion = avion;
+        this.estadoVuelo = "Programado";
+        this.puertaEmbarque = null;
+        this.tripulacion = tripulacion;
+        this.pilotos = pilotos;
+        this.reservas = new Reserva[0];
+    }
+
+    // Setters
+    public void setAvion(Avion avion){
+        this.avion = avion;
+    }
+
+    public void setPuertaEmbarque(String puertaEmbarque){
+        this.puertaEmbarque = puertaEmbarque;
+        this.confirmado();
+    }
+
+    public void setAtrado(LocalDateTime NewSalida){
+        this.fechaHoraSalida = NewSalida;
+        this.fechaHoraLlegada = null; //TODO
+        this.atrasado();
+    }
+
+    // Estado de vuelo
+    public void cancelado(){this.estadoVuelo="Cancelado";}
+    public void confirmado(){this.estadoVuelo="Confirmado";}
+    public void atrasado(){this.estadoVuelo="Atrasado";}
+    public void terminado(){this.estadoVuelo="Terminado";}
 }
+
